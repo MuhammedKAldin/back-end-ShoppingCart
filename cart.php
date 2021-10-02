98439f9f662af8dab3c6b11084c15450d681415a
MuhammedKAldin <54613628+MuhammedKAldin@users.noreply.github.com>
2021-10-02 09:50:10 +0200

    Update cart.php

--- cart.php
+++ cart.php
@@ -1,159 +1,103 @@
 <?php
-
 /**
  * Rest API to handle cart product adding :-
  * test : http://localhost/pape/cart.php?product=T-shirt+Blouse+Pants+Shoes+Jacket 
  * test - Jacket : http://localhost/pape/cart.php?product=Jacket
  * 
 */
-
-// getting JSON file into PHP
-$json = file_get_contents('cart.json');
-// decoding data in JSON
-$createCart = json_decode($json);
-
-
-if(isset($_GET['product']) && !empty($_GET['product']))
-{
+include 'class\Buy.php' ;
+include 'class\Checkout_Proccess.php' ;
+include 'class\ItemsDiscount_Proccess.php' ;
+if(isset($_GET['product']) && !empty($_GET['product'])) {
     $product = $_GET["product"];
     $buyList = explode( ' ' , $product);
-    
-    class Buy {
-
-        public static function getInstance()
-        {
-            static $instance = null;
-            if(null === $instance)
-            {
-                $instance = new static();
-            }
-            return $instance;
-        }
-        public $itemType;
-        public $itemPrice;
-        public $itemRegion;
-        public $itemWeight;
-    
-        // 10 dollars off shiiping for products >= 2 from total shipping price
-        public $shippingDiscountOffer = false;
-    
-        public function searchForDiscount($usedItem)
-        {
-            return $createCart->discount->$usedItem ;
-        }
-    }
-    
+    //-JSON-//
+    $json = file_get_contents('cart.json');
+    $createCart = json_decode($json);
+    //-----//
     // Div start-line
     echo "<div align='left' >";
+    $preTotal = 0; // items-Total price
+    $Totalshipping = 0; // items-Total shipping price
+    $VAT = 0.14; // Value Added Tax 14%
+    $netTotal = 0; // pretotal + VAT($pretotal)
+    $index = 0; // items counter variable
+    $discountsTotalValue = 0; 
+    $topGroupCount = 0; 
+    $jacketGroupCount = 0; 
+    // Looping bought items in seperate div
     echo '</br>'.'~~~~~~~~~~~~  Cart  ~~~~~~~~~~~~~'.'</br>';
-    
-    // created Instance from BUY Class to store values inside
-    $buyCart = Buy::getInstance();
-    
-    // calulations data
-    $Subtotal = 0;
-    $Shipping = 0;
-    $VAT = 0.14; // applied before discounts
-    $discountsApplied = 0;
-    $Total = 0;
-    // group calculations
-    $shoeGroup = 0; // gathering all shoeses and combining thier discounts
-    $topsGroup = 0;
-    $jacketsGroup = 0;
-    
-    $index = 0;
-    echo "<label>Added products:-</label></br>";
-    foreach ($buyList as $itemType)
-    {
-        //searching through the JSON 
-        $buyCart->itemType = $itemType;
-        $buyCart->itemPrice = $createCart->product->$itemType->price;
-        $buyCart->itemRegion = $createCart->product->$itemType->region;
-        $buyCart->itemWeight = 1000 * $createCart->product->$itemType->weight;
-    
-        //calculate inovice
-        $Subtotal += $buyCart->itemPrice;
-        //local variable to store current region
-        $getItemRegion = $buyCart->itemRegion;
-        // calculate item region
-        $Shipping += $buyCart->itemWeight / 100 * $createCart->region->$getItemRegion;
-    
-        if($itemType == "Shoes" ){$buyCart->shoesExist = true;}
-        if($itemType == "T-shirt" ){ $topsGroup += 1;}
-        if($itemType == "Blouse"  ){ $topsGroup += 1;}
-        if($itemType == "Jacket" ){ $jacketsGroup += 1;}
-        if($index >= 1){ $buyCart->shippingDiscountOffer = true;}
-    
-        // incrementing the current item index
+    foreach($buyList as $item) {
+        // created object from Buy Class
+        $buyCart = new Buy($item);
+        $topGroupCount += $buyCart->topGroupCount;
+        $jacketGroupCount += $buyCart->jacketGroupCount;
+        //$buyCart->itemIndex += $index; // ?????????????? why doesnt work
+        $buyProcces = new Checkout_Proccess();
+        $preTotal += $buyProcces->pre_totalPrice($buyCart);
+        $Totalshipping +=  $buyProcces->pre_totalShipping($buyCart);
+        $itemDiscountProccess = new ItemsDiscount_Proccess($buyCart,$topGroupCount,$jacketGroupCount);
+        $discountsTotalValue += $itemDiscountProccess->itemDiscountPrice;
+
+        // loop item index
         $index += 1;
-        // print item
-        echo $index.' - '.$buyCart->itemType;
-        echo "</br>";
     }
-    echo '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'.'</br>';  
-    echo 'Subtotal : $'.$Subtotal."</br>";
-    echo 'Shipping : $'.$Shipping."</br>";
-    echo 'VAT : $'.$Subtotal*$VAT."</br>";
-    if($discountsApplied != 0)
-    {
-        // if no discounts available then hide
+    // setting up the final calculations VAT & netTotal then show client inovice
+    $VAT = $VAT * $preTotal;
+    $multipleProducts = false;
+    // checking if items bought >= 2 items
+    if($index >= 2) {
+        // checking if the offer is available
+        if($createCart->product->multipleProducts == true) {
+            $multipleProducts = true;
+        }
+    }
+    // Inovice-Div start-line
+    echo "<div style='position:absolute;top: 44px;left: 145px; ' >";
+    echo 'Subtotal : $'.$preTotal."</br>";
+    echo 'Shipping : $'.$Totalshipping."</br>";
+    echo 'VAT : $'.$VAT."</br>";
+    //echo $topGroupCount." Tops  -  ".$jacketGroupCount." Jackets </br>"; // debugging
+    if($discountsTotalValue > 0  || $multipleProducts == 1){
+        /**(if no discounts then hide)
+        * Discounts-Panel 
+        */
         echo "Discounts : </br>";
-    }
-    echo "<div style='padding-left: 20px;'> ";
-    
-    foreach($buyList as $value)
-    {
-        if($createCart->product->$value->discountExist == true)
-        {
-            if($value == "Shoes" ) {
-            /// shoes-discount
-            /// incase there is multiple selection of shoeses, calculate thier discount and apply them once
-            $shoeGroup += 1;
-            }
-            // jacket-discount
-            // checking if the T-shirt , Blouse and Jacket Existing
-            if($value == "Jacket")
-            {
-                if($topsGroup >= 2 && $jacketsGroup >= 1)
-                {
-                    $discountsApplied  +=($createCart->product->$value->discount * $createCart->product->$value->price);
-                    echo ($createCart->product->$value->discount*100)."% off jacket: -$".($createCart->product->$value->discount * $createCart->product->$value->price)."</br>";     
-                    $topsGroup -= 2;
-                    $jacketsGroup -= 1;
-    
+        echo "<div style='padding-left: 20px;'> ";
+        // (*) Items Offers (*)
+        if($topGroupCount > 0){
+            foreach($buyList as $item) {
+                if($item == "Jacket" && $topGroupCount >= 2 && $jacketGroupCount >= 1) {
+                    echo ($createCart->product->$item->discount*100)."% off ".$item.": -$".($createCart->product->$item->discount * $createCart->product->$item->price)."</br>";     
+                    $topGroupCount -= 2; // took offer
+                    $jacketGroupCount -= 1; // took offer
+                }
+                if($item == "Shoes") {
+                    echo ($createCart->product->$item->discount*100)."% off ".$item.": -$".($createCart->product->$item->discount * $createCart->product->$item->price)."</br>";     
                 }
             }
-    
         }
-    
+        // (*) Seasonal Offers (*)
+        if($multipleProducts == 1) {
+            // Apply -10$ Dollars off shipping
+            $offerValue = $createCart->product->multipleProducts->discount;
+            $Totalshipping -= $offerValue;
+            echo $offerValue."$ of shipping: -$".$offerValue."</br>";
+        }
+        // Discounts-div end-line
+        echo "</div>";
     }
-    
-    if($shoeGroup > 0)
-    {
-        $discountsApplied  +=($createCart->product->Shoes->discount * $createCart->product->Shoes->price);
-        echo ($createCart->product->Shoes->discount*100*$shoeGroup)."% off shoes: -$".($createCart->product->Shoes->discount * $createCart->product->Shoes->price)*$shoeGroup."</br>";
-    }
-    
-    
-    if($buyCart->shippingDiscountOffer == true)
-    {
-        // two or more items in cart discount
-        $Shipping -= $createCart->product->multipleProducts->discount; 
-        echo ($createCart->product->multipleProducts->discount)."$ of shipping: -$".$createCart->product->multipleProducts->discount."</br>";
-    
-    }
-    
+    $netTotal = $preTotal + $Totalshipping + $VAT;
+    echo "Total : $".($netTotal - $discountsTotalValue);
+    // Inovice-Div end-line
     echo "</div>";
-    $totalApplied_VAT = $Subtotal*$VAT;
-    $Total = ($Subtotal + $totalApplied_VAT + $Shipping);
-    echo 'Total : $'.($Total-$discountsApplied)."</br>";
-    
     // Div end-line
     echo "</div>";
 } else {
-    echo "the shopping cart is empty";
+    writeIn('the shopping cart is empty');
 }
-
-
-
+// printing method
+function writeIn($line_in) {
+    echo "</br>".$line_in."</br>";
+}
 ?>
